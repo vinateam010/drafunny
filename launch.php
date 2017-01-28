538729da749b08d1d87df2551e72b50099d30df4
vinateam010 <cftestvn0@gmail.com>
2017-01-28 08:02:39 +0700

    Fist

---
+++ launch.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="utf-8">
+    <title></title>
+	<style>
+	  html {
+		scrollbar-face-color: #171508;
+		scrollbar-arrow-color: #171508;
+		scrollbar-track-color: #171508;
+		scrollbar-highlight-color: #171508;
+		scrollbar-shadow-color: #171508;
+		scrollbar-base-color: #171508;
+	  }
+	body{
+		color: #171508;
+		padding:0;
+		margin:0;
+	}
+	.windowbg2{
+		background: #171508;
+	}
+	.article_text {
+		padding: 10px;
+	}
+	.article_inner {
+		line-height: 1.4em;
+	}
+	</style>
+  </head>
+  <body>
+    <div class="windowbg2">
+				<div class="article_padding article_text" style="clear: both;">
+					<div class="article_inner">
+
+<span class="text_exposed_show" style="display: inline;"><b style="color: white;">---><img src="http://wap.mobifone.com.vn/wap/xhtml/img/Icon_hot.png"><--- Dragonica Funny Reborn ---><img src="http://wap.mobifone.com.vn/wap/xhtml/img/Icon_hot.png"><---<br>
+•><img src="http://wap.mobifone.com.vn/wap/xhtml/img/Icon_hot.png"><• Mở Cửa Thử Nghiệm: 00:00 ngày 28/1 •><img src="http://wap.mobifone.com.vn/wap/xhtml/img/Icon_hot.png"><•<br>
+
+
+		--------------------------------------------------<br>
+		Trang chủ: <a href="http://drafunny.com">Tham Gia</a><br>
+		Tải game ngay tại: <a href="http://drafunny.com/tai-game#download">Tham Gia</a><br>
+		Đăng ký: <a href="http://drafunny.com/tai-khoan/dang-ky">Tham Gia</a><br>
+		Tham gia diễn đàn, góp ý cho server: <a href="http://drafunny.forumvi.com/">Tham Gia</a><br>
+		Fanpage: <a href="https://www.facebook.com/drafunny">Tham Gia</a><br>
+		Group: <a href="https://www.facebook.com/groups/hbfdragonica/">Tham Gia</a><br>
+        <iframe scrolling="no" width="640" height="180" src="http://mp3.zing.vn/embed/song/IWB9UBEA" frameborder="0" allowfullscreen="true"></iframe>
+			</b></span></div><b style="color: white;">
+			
+			</b></div><b style="color: white;">
+		</b>
+		</div>
+  </body>
+</html>
